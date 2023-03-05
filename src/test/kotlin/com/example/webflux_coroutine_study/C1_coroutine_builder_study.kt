ecb2c52e3db60686d87bf0a224edbe4f4bb387c9
jinia91 <creee77@gmail.com>
2023-03-06 00:46:30 +0900

    코루틴 스코프 이해

--- src/test/kotlin/com/example/webflux_coroutine_study/C1_coroutine_builder_study.kt
+++ src/test/kotlin/com/example/webflux_coroutine_study/C1_coroutine_builder_study.kt
@@ -1,11 +1,11 @@
 package com.example.webflux_coroutine_study
 
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
 import org.junit.jupiter.api.Test
-import kotlin.coroutines.coroutineContext
 
 private val log = KotlinLogging.logger { }
 
@@ -89,7 +89,7 @@ class C1_coroutine_builder_study {
     fun `suspend 사용해보기`() {
         val result = runBlocking<Long> {
             log.info { "1. runBlocking : $this" }
-            launch {
+            launch {// 코루틴 스코프 생성!!, reciever. 수신객체
                 doTwo()
             }
             launch {
@@ -117,6 +117,118 @@ class C1_coroutine_builder_study {
     private suspend fun doThree(){
         delay(1000)
         log.info { "4. launch 3 " }
+    }
+
+    /**
+     * suspend : suspension Function을 칭하는 의미일 뿐, 코루틴이 아님
+     *
+     * 단순히 잠들수 있다는걸 알려주는것일 뿐
+     */
+
+    @Test
+    fun `코루틴 아닌곳에서 suspend Fucntion을 호출하면`(){
+//        doOne()
+        // 컴파일실패
+
+        /**
+         * - runBlocking은 현재 쓰레드를 멈추게 만들고, 기다리지만 coroutineScope는 현재 스레드를 멈추게 하지 않음
+         *
+         * - coroutineScope는 launch처럼 협력하여 작동가능
+         */
+//        coroutineScope {
+//            launch {
+//                doOne()
+//            }
+//            launch {
+//                doTwo()
+//            }
+//            launch {
+//                doThree()
+//            }
+//        }
+
+        runBlocking {
+            // luanch는 job을 반환!
+            val job = launch {
+                doOne()
+            }
+
+            job.join() // 기다리게하기 , suspension point
+
+            val job2 = launch {
+                doTwo()
+            }
+
+            launch {
+                doThree()
+            }
+        }
+    }
+
+    /**
+     * 코루틴 부모자식 이해하기
+     *
+     * 부모 코루틴 1
+     *
+     * ㄴ 자식코루틴 1(launch)
+     *
+     * ㄴ 자식코루틴 2(launch)
+     *
+     * ㄴ 자식코루틴 3(launch)
+     *
+     * 자식 2에서 지연을 주더라도 부모1은 자식3실행후 자식2가 끝날때까지 기다림
+     *
+     * 부모 코루틴 2
+     *
+     * ㄴ 자식코루틴 _1(launch)
+     *
+     * ㄴ 자식코루틴 _2(launch)
+     *
+     */
+    @Test
+    fun `코루틴 부모자식 이해하기`() {
+        runBlocking {
+            parent1()
+            parent2()
+        }
+    }
+
+    private suspend fun parent1() = coroutineScope {
+        launch {
+            child1()
+        }
+        launch {
+            child2()
+        }
+        launch {
+            child3()
+        }
+    }
+
+    private suspend fun child1() = log.info { "애기 응애" }
+    private suspend fun child2()  {
+        delay(1000)
+        log.info { "애기2 응애지만 3보다 늦게" }
+    }
+
+    private suspend fun child3()  {
+        log.info { "애기3 응애" }
+    }
+
+    private suspend fun parent2() = coroutineScope {
+        launch {
+            child_1()
+        }
+        launch {
+            child_2()
+        }
+    }
+
+    private suspend fun child_1()  {
+        log.info { "애기_1 응애" }
+    }
+    private suspend fun child_2()  {
+        log.info { "애기_2 응애" }
     }
 
 }
